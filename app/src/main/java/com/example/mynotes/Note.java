257eeb29014553f13c17b60168a6a4eada7e97a0
Tanner <tdyer@hawk.iit.edu>
2020-09-23 10:46:52 -0500

    created note class for representing notes

---
+++ app/src/main/java/com/example/mynotes/Note.java
@@ -0,0 +1,14 @@
+package com.example.mynotes;
+
+public class Note {
+
+    String note;
+    String title;
+    String lastSave;
+
+    public Note(String note, String title, String lastSave) {
+        this.note = note;
+        this.title = title;
+        this.lastSave = lastSave;
+    }
+}
